a30a9f784a7a4d6ae2b61040ee0d236e5a678d32
Muhammadirfan785 <220660121160@student.unsap.ac.id>
2024-12-20 17:18:40 +0700

    commit

---
+++ 220660121160/Praktikum-JS/object.js
@@ -0,0 +1,17 @@
+//contoh implementasi objek
+
+//notasi objek
+var person = {
+    nama: 'Muhammad Irfan',
+    umur: 21,
+    alamat: 'Jl. Raya No. 1'
+};
+
+//associative-array
+var orang = {};
+orang['nama'] = 'Muhammad Irfan';
+orang['umur'] = 20;
+orang['alamat'] = 'Dusun Bungbulang';
+
+console.log(person);
+console.log(orang);
